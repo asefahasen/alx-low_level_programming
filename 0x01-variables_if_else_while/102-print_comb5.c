82280426ac505110f678b47c8362d3a13d578329
asefahasen <asefa.ali@aau.edu.et>
2022-06-03 06:15:54 -0700

    asefa

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-
+/**
+ * main - create 2 pairs of number
+ * Return: 0
+ */
 int main(void)
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i <= 99; i++)
-    {
-      for (j = i; j <= 99; j++)
+	for (i = 0; i <= 99; i++)
 	{
-	  if (j != i)
-	    {
-	      putchar(i / 10 + 48);
-	      putchar(i % 10 + 48);
-	      putchar(' ');
-	      putchar(j / 10 + 48);
-	      putchar(j % 10 + 48);
-
-	      if (i * 100 + j != 9899)
+		for (j = i; j <= 99; j++)
 		{
-		  putchar(',');
-		  putchar(' ');
+			if (j != i)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	    }
 	}
-    }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
